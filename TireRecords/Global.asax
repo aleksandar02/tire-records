c96c218db4c96abc8435046125013cc64ef475f7
aleksandar02 <aca.milic02@gmail.com>
2020-02-20 19:43:29 +0100

    Add project files.

---
+++ TireRecords/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TireRecords.MvcApplication" Language="C#" %>
---
+++ TireRecords/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TireRecords.Startup))]
+namespace TireRecords
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
