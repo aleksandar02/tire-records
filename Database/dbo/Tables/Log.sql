7d29f0dd49bfe394dfa461f43da5472e85328f1f
joksic <joksic@protonmail.com>
2020-03-08 01:07:47 +0100

    Log table added to Database project.

---
+++ Database/dbo/Tables/Log.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Log](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[TimeStamp] [datetime] NOT NULL,
+	[Level] [varchar](100) NOT NULL,
+	[Logger] [varchar](1000) NOT NULL,
+	[Message] [varchar](3600) NOT NULL,
+	[UserId] [int] NULL,
+	[Exception] [varchar](3600) NULL,
+	[StackTrace] [varchar](3600) NULL,
+ CONSTRAINT [PK_ExceptionLog] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
