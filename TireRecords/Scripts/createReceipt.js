04b67ccb01a9b674c19624d746b127ba95b9993f
aleksandar02 <aca.milic02@gmail.com>
2020-03-18 15:24:10 +0100

    Change from top navigation to side navigation.

---
+++ TireRecords/Scripts/createReceipt.js
@@ -0,0 +1,106 @@
+﻿$(document).ready(function () {
+    var vehicleBrands = [
+        "Abarth",
+        "Alfa Romeo",
+        "Aston Martin",
+        "Audi",
+        "Bentley",
+        "BMW",
+        "Bugatti",
+        "Cadillac",
+        "Chevrolet",
+        "Chrysler",
+        "Citroën",
+        "Dacia",
+        "Daewoo",
+        "Daihatsu",
+        "Dodge",
+        "Donkervoort",
+        "DS",
+        "Ferrari",
+        "Fiat",
+        "Fisker",
+        "Ford",
+        "Honda",
+        "Hummer",
+        "Hyundai",
+        "Infiniti",
+        "Iveco",
+        "Jaguar",
+        "Jeep",
+        "Kia",
+        "KTM",
+        "Lada",
+        "Lamborghini",
+        "Lancia",
+        "Land Rover",
+        "Landwind",
+        "Lexus",
+        "Lotus",
+        "Maserati",
+        "Maybach",
+        "Mazda",
+        "McLaren",
+        "Mercedes-Benz",
+        "MG",
+        "Mini",
+        "Mitsubishi",
+        "Morgan",
+        "Nissan",
+        "Opel",
+        "Peugeot",
+        "Porsche",
+        "Renault",
+        "Rolls-Royce",
+        "Rover",
+        "Saab",
+        "Seat",
+        "Skoda",
+        "Smart",
+        "SsangYong",
+        "Subaru",
+        "Suzuki",
+        "Tesla",
+        "Toyota",
+        "Volkswagen",
+        "Volvo"
+    ];
+
+    var container = $('#vehicleBrandPicker');
+    console.log(container);
+
+    $.each(vehicleBrands, function (index, item) {
+        container.append(`<option value="${item.trim()}">
+                                       ${item.trim()}
+                                  </option>`);
+    });
+
+    $('.selectpicker').selectpicker({
+        liveSearch: true,
+        showSubtext: true,
+        noneResultsText: "Nema rezultata!"
+    });
+
+
+    $('.copy-tire').click(function () {
+        var id = $(this).closest('.row').attr('id');
+
+        var source = $('#' + id + ' input');
+        var rows = $('#tireSection .row');
+
+        copyToNextRow(source, rows);
+    });
+
+    function copyToNextRow(source, rows) {
+        for (var i = 0; i < 4; i++) {
+            if (rows[i].children[1].children[0].value === '') {
+
+                for (var j = 1; j < 7; j++) {
+                    rows[i].children[j].children[0].value = source[j].value;
+                }
+
+                return;
+            }
+        }
+    }
+});
